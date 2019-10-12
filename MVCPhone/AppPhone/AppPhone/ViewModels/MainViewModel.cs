f991d6d6b9dfb86af9648ca95231a1c1f7b8bea2
visual_community@outlook.com <visual_community@outlook.com>
2019-10-12 13:17:07 -0300

    MVVM Completo

---
+++ MVCPhone/AppPhone/AppPhone/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using AppPhone.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppPhone.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Properties
+        public List<Phone> ListPhone { get; set; }
+        #endregion
+
+        #region ViewModel
+        PhoneBookViewModel phoneBookViewModel { get; set; }
+        #endregion
+
+
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            phoneBookViewModel = new PhoneBookViewModel();
+        } 
+        #endregion
+
+
+        #region Singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if (instance==null)
+            {
+                instance = new MainViewModel();      
+            }
+            return (instance);
+        }
+        #endregion
+
+
+    }
+}
---
+++ MVCPhone/AppPhone/AppPhone/ViewModels/PhoneBookViewModel.cs
@@ -0,0 +1,83 @@
+﻿using AppPhone.Models;
+using AppPhone.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace AppPhone.ViewModels
+{
+    public class PhoneBookViewModel: BaseViewModel
+    {
+        #region Attributes
+        private ApiService apiService;
+        ObservableCollection<Phone> phones;
+        #endregion
+
+        #region Properties
+
+        ObservableCollection<Phone> Phones
+        {
+            get { return this.phones; }
+            set { SetValue(ref this.phones, value);}
+
+        }
+        #endregion
+
+        #region Constructor
+        public PhoneBookViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadPhone();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadPhone()
+        {
+            var connection = await apiService.CheckConnection();
+            if(!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Internet Error Connection", connection.Message,"Accept");
+                return;
+            }
+            var response = await apiService.GetList<Phone>(
+                "http://localhost:50310/",
+                "api/",
+                "Phones"
+                );
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error Phone Service",
+                    response.Message,
+                    "Accept"
+                    );
+                return;
+            }
+            MainViewModel main = MainViewModel.GetInstance();
+            main.ListPhone = (List<Phone>) response.Result;
+
+            this.Phones = new ObservableCollection<Phone>(ToPhoneCollect());
+        }
+
+        private IEnumerable<Phone> ToPhoneCollect()
+        {
+            ObservableCollection<Phone> collection = new ObservableCollection<Phone>();
+            MainViewModel main = MainViewModel.GetInstance();
+            foreach (var lista in main.ListPhone)
+            {
+                Phone phone = new Phone();
+                phone.PhoneID = lista.PhoneID;
+                phone.Name = lista.Name;
+                phone.Type = lista.Type;
+                phone.Contact = lista.Contact;
+                collection.Add(phone);
+            }
+            return (collection);
+        }
+        #endregion
+
+
+    }
+}
